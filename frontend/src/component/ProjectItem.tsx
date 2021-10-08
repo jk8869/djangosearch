e54a8475e9701442f9277133739e731291793ac2
karimi <jafarkarimi.1369@gmail.com>
2021-10-08 21:15:01 +0330

    add product tile for product list

--- frontend/src/component/ProjectItem.tsx
+++ frontend/src/component/ProjectItem.tsx
@@ -10,94 +10,38 @@ type ProjectItemProps = {
 
 export default function ProjectItem({ project } : ProjectItemProps) {
   return (
-    <main className="singleProject my-md">
-      <div className="container">
-        <div className="layout">
-          <div className="column column--1of3">
-            <h3 className="singleProject__subtitle">Tools & Stacks</h3>
-            <div className="singleProject__toolStack">
-
+    <div className="column">
+      <div className="card project">
+        <a href="{% url 'project' project.id %}" className="project">
+          <img className="project__thumbnail" src="{{project.featured_image.url}}" alt="project thumbnail" />
+          <div className="card__body">
+            <h3 className="project__title">{project.title}</h3>
+            <p><a className="project__author" href="{% url 'project' project.id %}">By Dennis Ivanov</a></p>
+            <p className="project--rating">
+              <span style={{ fontWeight: 'bold' }}>
+                {project.vote_ratio}
+                %
+              </span>
+              {' '}
+              Positive
+              Feedback (
+              {project.vote_total}
+              {' '}
+              Vote
+              {project.vote_total}
+              )
+            </p>
+            <div className="project__tags">
               {project.tags.map((tag, index) => (
-                <span className="tag tag--pill tag--sub tag--lg">
+                <span className="tag tag--pill tag--main">
                   <small>{tag}</small>
                 </span>
-              ))}
+              )) }
 
-            </div>
-            {project.source_link ? (
-              <a className="singleProject__liveLink" href="#" target="_blank">
-                <i className="im im-external-link" />
-                Source Code
-              </a>
-            ) : null}
-
-          </div>
-          <div className="column column--2of3">
-            <img className="singleProject__preview" src="{{ project.featured_image.url }}" alt="portfolio thumbnail" />
-            <a href="profile.html" className="singleProject__developer">Md. Shahriar Parvez</a>
-            <h2 className="singleProject__title">{project.title}</h2>
-            <h3 className="singleProject__subtitle">About the Project</h3>
-            <div className="singleProject__info">
-              {project.descriptions}
-            </div>
-
-            <div className="comments">
-              <h3 className="singleProject__subtitle">Feedback</h3>
-              <h5 className="project--rating">
-                {project.vote_ratio}
-                % Postitive Feedback (
-                {project.vote_total}
-                ) Vote
-                {project.vote_total}
-              </h5>
-
-              <form className="form" action="#" method="POST">
-                <div className="form__field">
-                  <label htmlFor="formInput#textarea">Comments: </label>
-                  <textarea
-                    className="input input--textarea"
-                    name="message"
-                    id="formInput#textarea"
-                    placeholder="Write your comments here..."
-                  />
-                </div>
-                <input className="btn btn--sub btn--lg" type="submit" value="Comments" />
-              </form>
-              <div className="commentList">
-                <div className="comment">
-                  <a href="profile.html">
-                    <img
-                      className="avatar avatar--md"
-                      src="https://pbs.twimg.com/profile_images/1335382240931368961/b3wSZKj4_400x400.jpg"
-                      alt="user"
-                    />
-                  </a>
-                  <div className="comment__details">
-                    <a href="profile.html" className="comment__author">Sulamita Ivanov</a>
-                    <p className="comment__info">
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit alias numquam perferendis
-                      mollitia minus minima exercitationem possimus ab deserunt qui, soluta iusto doloribus eveniet
-                      similique consequuntur ratione, dignissimos ut magni laborum quo.
-                    </p>
-                  </div>
-                </div>
-                <div className="comment">
-                  <a href="profile.html">
-                    <img className="avatar avatar--md" src="https://avatars.githubusercontent.com/u/33843378" alt="user" />
-                  </a>
-                  <div className="comment__details">
-                    <a href="profile.html" className="comment__author">Dennis Ivanov</a>
-                    <p className="comment__info">
-                      Consectetur adipisicing elit. Reprehenderit alias numquam perferendis mollitia minus minima
-                      exercitationem possimus ab deserunt qui, ratione, dignissimos ut magni laborum quo.
-                    </p>
-                  </div>
-                </div>
-              </div>
             </div>
           </div>
-        </div>
+        </a>
       </div>
-    </main>
+    </div>
   );
 }
--- frontend/src/page/ProjectList.tsx
+++ frontend/src/page/ProjectList.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import ProjectItem from '../component/ProjectItem';
@@ -25,8 +26,49 @@ export default function ProjectList() {
   }, []);
 
   return (
-    <div>
-      {projects?.map((project: Project) => (<ProjectItem key={project.id} project={project} />))}
-    </div>
+
+    <main className="projects">
+      <section className="hero-section text-center">
+        <div className="container container--narrow">
+          <div className="hero-section__box">
+            <h2>
+              Search for
+              {' '}
+              <span>Projects</span>
+            </h2>
+          </div>
+
+          <div className="hero-section__search">
+            <form className="form" action="#" method="get">
+              <div className="form__field">
+                <label htmlFor="formInput#search">Search By Projects </label>
+                <input
+                  className="input input--text"
+                  id="formInput#search"
+                  type="text"
+                  name="text"
+                  placeholder="Search by Project Title"
+                />
+              </div>
+
+              <input className="btn btn--sub btn--lg" type="submit" value="Search" />
+            </form>
+          </div>
+        </div>
+      </section>
+      <section className="projectsList">
+        <div className="container">
+          <div className="grid grid--three">
+            {projects?.map((project: Project) => (
+              <ProjectItem
+                key={project.id}
+                project={project}
+              />
+            ))}
+
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
