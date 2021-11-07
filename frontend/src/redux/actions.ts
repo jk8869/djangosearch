0c08bfa1f2d3042ef8e04d26087248240144464f
Jafar Karimi <jafarkarimi.1369@gmail.com>
2021-11-07 09:24:12 +0330

    start to add react-saga

--- frontend/src/redux/actions.ts
+++ frontend/src/redux/actions.ts
@@ -1,7 +1,8 @@
 import { Project } from '../interface/Project';
 import { Action } from './reducers';
+import {PROJECT_FETCH_SUCCEEDED, PROJECT_LIST_FETCH_FAILED, PROJECT_LIST_FETCH_REQUESTED, PROJECT_LIST_FETCH_SUCCEEDED} from './constants';
 
 export const fillProjectsList = (projects: Project[]): Action => ({
-  type: 'FILL_LIST',
+  type: PROJECT_LIST_FETCH_REQUESTED,
   payload: projects,
 });
--- frontend/src/redux/reducers.ts
+++ frontend/src/redux/reducers.ts
@@ -1,4 +1,5 @@
 import { Project } from '../interface/Project';
+import {PROJECT_FETCH_SUCCEEDED, PROJECT_LIST_FETCH_FAILED, PROJECT_LIST_FETCH_REQUESTED, PROJECT_LIST_FETCH_SUCCEEDED} from './constants';
 
 export interface ProjectsState {
     projects: Project[];
@@ -6,16 +7,20 @@ export interface ProjectsState {
 
 const initialState = {
   projects: [],
+  project: null
 };
 
-export type Action = {type: 'FILL_LIST', payload: Project[]};
+export type Action = {type: String, payload: Project[]};
 
 export const projectReducer = (
   state: Project[] = initialState.projects,
   action: Action
 ) => {
   switch (action.type) {
-    case 'FILL_LIST': {
+    case PROJECT_LIST_FETCH_REQUESTED: {
+      return { ...state, projects: [] };
+    }
+    case PROJECT_LIST_FETCH_SUCCEEDED: {
       return { ...state, projects: [action.payload] };
     }
     default:
