7af294295147da133161298b6d32597b19e2eddb
karimi <jafarkarimi.1369@gmail.com>
2021-11-26 20:48:12 +0330

    add saga

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Project from './page/Project';
@@ -10,8 +11,13 @@ import { projectReducer } from './redux/reducers';
 import ProjectList from './page/ProjectList';
 import Navbar from './component/Navbar';
 import ProjectForm from './page/ProjectForm';
+import { mySaga } from './redux/sagas';
 
-const store = createStore(projectReducer);
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(projectReducer, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(mySaga);
 
 ReactDOM.render(
   <React.StrictMode>
---
+++ frontend/src/redux/sagas.ts
@@ -0,0 +1,20 @@
+import { call, put, takeEvery, takeLatest } from 'redux-saga/effects';
+import { Api } from '../helper/Api';
+import { PROJECT_LIST_ITEM_SELECTE, PROJECT_LIST_FETCH_FAILED,
+  PROJECT_LIST_FETCH_REQUESTED, PROJECT_LIST_FETCH_SUCCEEDED,
+  TAG_LIST_FETCH_REQUESTED,
+  TAG_LIST_FETCH_SUCCEEDED } from './constants';
+
+function* fetchProjects(action: any) {
+  try {
+    const api = new Api();
+    const projectList = yield call(api.getProjects);
+    yield put({ type: PROJECT_LIST_FETCH_SUCCEEDED, projects: projectList });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+export function* mySaga() {
+  yield takeEvery(PROJECT_LIST_FETCH_REQUESTED, fetchProjects);
+}
